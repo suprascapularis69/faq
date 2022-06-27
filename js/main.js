11e518bdd8c2c7d2a4116cd66277eb8072223b80
suprascapularis69 <86835360+suprascapularis69@users.noreply.github.com>
2022-06-27 19:10:35 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,10 +1,24 @@
-let sections = document.querySelectorAll("details");
-sections.forEach((section) => section.addEventListener('toggle', toggleAcordeon));
+let listHeaders = document.querySelectorAll('.list-header');
 
-function toggleAcordeon() {
-  this.open && sections.forEach((section) => {
-    if (section !== this && section.open) {
-      section.open = false;
+listHeaders.forEach((elem) => {
+  elem.addEventListener('click', function() {
+    let parentElem = this.parentNode;
+    let listContent = parentElem.querySelector('.list-content');
+    let arrowDown = parentElem.querySelector('.arrow-down');
+    let headerText = parentElem.querySelector('.header-text');
+    if(listContent.classList.contains('.active') | arrowDown.classList.contains('active') | headerText.classList.contains('active')){
+    listContent.classList.remove('active');
+    arrowDown.classList.remove('active');
+    headerText.classList.remove('active');
+    } else {      
+      if (document.querySelector('.active')) {
+        for (let x of document.querySelectorAll('.active')) {
+          x.classList.remove('active');
+        }
+      }
+    listContent.classList.add('active');
+    arrowDown.classList.add('active');
+    headerText.classList.add('active');
     }
-  });
-}
+  })
+})
